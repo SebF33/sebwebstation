f0ca6b3171aa34dc025c8b03da51f901c2387c21
SebF33 <78741537+SebF33@users.noreply.github.com>
2025-04-25 11:03:09 +0200

    const technologies

---
+++ src/utils/constants.js
@@ -0,0 +1,41 @@
+export const technologies = [{
+    name: "PHP",
+    icon: "./img/php.svg"
+  },
+  {
+    name: "Symfony",
+    icon: "./img/symfony.svg"
+  },
+  {
+    name: "JavaScript",
+    icon: "./img/javascript.svg"
+  },
+  {
+    name: "Vue.js",
+    icon: "./img/vuejs.svg"
+  },
+  {
+    name: "TypeScript",
+    icon: "./img/typescript.svg"
+  },
+  {
+    name: "Deno",
+    icon: "./img/deno.svg"
+  },
+  {
+    name: "Fresh",
+    icon: "./img/fresh.svg"
+  },
+  {
+    name: "React",
+    icon: "./img/react.svg"
+  },
+  {
+    name: "Three.js",
+    icon: "./img/threejs.svg"
+  },
+  {
+    name: "Tailwind CSS",
+    icon: "./img/tailwindcss.svg"
+  },
+];
