d28f552f32ad5c837e9b6b1805918685773a2d43
SebF33 <78741537+SebF33@users.noreply.github.com>
2025-05-03 12:52:19 +0200

    css update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,12 +8,12 @@ const Home = () => {
   return (
     <section className="relative w-full h-screen mx-auto overflow-hidden">
       <div
-        className={`absolute inset-0 top-[60px] md:top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[30px] md:top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-lighter-sith" />
           <div
-            className="w-1 sm:h-80 h-44
+            className="w-1 sm:h-80 h-52
              bg-gradient-to-b
              from-lighter-sith
              to-lighter-jedi"
